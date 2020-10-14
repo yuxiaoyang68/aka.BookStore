c2441cd52957fd396a570087c44e2bb6e924e058
stevenyu <yuxiaoyang@hotmail.com>
2020-10-14 14:23:28 -0400

    Add project files.

---
+++ Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+	public class Login
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+	}
+}
